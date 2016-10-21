b998b72e2906893af4933f157ca16a583633f051
Ashish Kumar <akbrother02@gmail.com>
2016-10-21 14:24:12 +0530

    Completed

--- app/src/main/java/com/example/aashish/marshmallowpermission/HomeActivity.java
+++ app/src/main/java/com/example/aashish/marshmallowpermission/HomeActivity.java
@@ -1,5 +1,11 @@
 package com.example.aashish.marshmallowpermission;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,14 +29,54 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         switch(view.getId())
         {
             case R.id.btn_permission:
-                if(isReadStorageAllowed()){
+                if(ReadMsgAllowed()){
                     //If permission is already having then showing the toast
                     Toast.makeText(HomeActivity.this,"You already have the permission",Toast.LENGTH_LONG).show();
                     //Existing the method with return
                     return;
                 }
+                requestReadMsgPermission();
+                        
 
         }
 
+    }
+
+    private void requestReadMsgPermission() {
+    if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_SMS)){
+
+    }
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_SMS},200);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==200)
+        {
+            if(grantResults.length>0&& grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(getApplicationContext(),"Permission Granted",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Permission Rejected",Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+    private boolean ReadMsgAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        //If permission is not granted returning false
+        return false;
     }
 }
