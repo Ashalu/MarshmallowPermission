b9f737199147048309d39a7a2b286ae632547a3f
Ashish Kumar <akbrother02@gmail.com>
2016-10-21 13:42:14 +0530

    Adding Button

--- app/src/main/java/com/example/aashish/marshmallowpermission/HomeActivity.java
+++ app/src/main/java/com/example/aashish/marshmallowpermission/HomeActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +23,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         switch(view.getId())
         {
             case R.id.btn_permission:
+                if(isReadStorageAllowed()){
+                    //If permission is already having then showing the toast
+                    Toast.makeText(HomeActivity.this,"You already have the permission",Toast.LENGTH_LONG).show();
+                    //Existing the method with return
+                    return;
+                }
 
         }
 
